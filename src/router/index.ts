d96be19ef79a0bd42fb786ac81962b21c1bb8f18
miao <miao@163.com>
2021-09-03 00:37:17 +0800

    添加路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,29 @@
 import Vue from 'vue';
 import VueRouter, {RouteConfig} from 'vue-router';
+import NotebookList from '@/views/NotebookList.vue';
+import Login from '@/views/Login.vue';
+import NoteDetail from '@/views/NoteDetail.vue';
+import TrashDetail from '@/views/TrashDetail.vue';
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
+  {
+    path: 'login',
+    component: Login
+  },
+  {
+    path: 'notebooks',
+    component: NotebookList
+  },
+  {
+    path: 'noteId',
+    component: NoteDetail
+  },
+  {
+    path: 'trash:noteId',
+    component: TrashDetail
+  }
 ];
 
 const router = new VueRouter({
