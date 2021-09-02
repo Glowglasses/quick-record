f89b77553b134a6a5b714c173a3236f0db2b1846
miao <miao@163.com>
2021-09-02 23:55:06 +0800

    添加css,reset

--- src/App.vue
+++ src/App.vue
@@ -8,25 +8,9 @@
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style lang="scss" scoped>
+  @import "~@/assets/scss/reset.scss";
+  body{
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
   }
-}
 </style>
