643c65e6df353fb5e3133cea479832f286d6cade
miao <miao@163.com>
2021-09-04 05:00:09 +0800

    完成请求函数

---
+++ src/helpers/request.ts
@@ -0,0 +1,31 @@
+import axios, {AxiosRequestConfig} from 'axios';
+
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.defaults.baseURL = process.env.VUE_APP_BASE_URL;
+axios.defaults.withCredentials = true;
+export default function request(url: string, type = 'GET', data = {}) {
+  return new Promise((resolve, reject) => {
+    const config: AxiosRequestConfig = {
+      url,
+      validateStatus(status) {
+        return (status >= 200 && status < 300) || status === 400;
+      }
+    };
+    if (type.toLocaleLowerCase('get')) {
+      config.params = data;
+      config.method = 'GET';
+    } else {
+      config.data = data;
+      config.method = 'POST';
+    }
+    axios(config).then(request => {
+      if (request.status === 200) {
+        resolve(request.data);
+      } else {
+        reject(request.data);
+      }
+    }).catch(() => {
+      reject({msg: '网络异常'});
+    });
+  });
+}
