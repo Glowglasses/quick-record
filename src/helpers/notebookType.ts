3b61e2023e4f474dc41c70e54004b381c1a5e8ab
miao <miao@163.com>
2021-09-05 01:37:54 +0800

    添加notebook声明

---
+++ src/helpers/notebookType.ts
@@ -0,0 +1,23 @@
+﻿export type notebook = {
+  id: number
+  title: string
+  userId:number
+  createdAt: string
+  updatedAt: string
+  noteCount: number
+  friendlyCreatedAt?: string
+}
+export type notebooks = notebook[]
+
+export type createNotebook = {
+  msg: string
+  data: notebook
+}
+
+export type updateNotebook = {
+  msg: string
+}
+
+export type deleteNotebook = {
+  msg: string
+}
