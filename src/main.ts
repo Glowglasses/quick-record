b9241efd0f0e4af769baa6159a2471a35babd840
miao <miao@163.com>
2021-09-04 23:32:57 +0800

    全局使用ToastUI组件

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import SlideBar from '@/components/SlideBar.vue';
+import {Toast} from 'vant';
 
 Vue.config.productionTip = false
 Vue.component("SlideBar",SlideBar)
+Vue.use(Toast)
 new Vue({
   router,
   store,
