81e7800662cf93008a19d28073e86cfd431b2269
miao <miao@163.com>
2021-09-07 01:39:29 +0800

    类组件注册路由钩子

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
 import SlideBar from '@/components/SlideBar.vue';
 import 'element-ui/lib/theme-chalk/index.css';
+import Component from 'vue-class-component';
 
-Vue.config.productionTip = false
-Vue.component("SlideBar",SlideBar)
+Component.registerHooks([
+  'beforeRouteEnter',
+  'beforeRouteLeave',
+  'beforeRouteUpdate'
+]);
+Vue.config.productionTip = false;
+Vue.component('SlideBar', SlideBar);
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
