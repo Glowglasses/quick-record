f88dee24a481df53ba06913cb96343f0c9919f3b
miao <miao@163.com>
2021-09-07 06:52:27 +0800

    添加Vuex的index文件

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,10 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import notebook from '@/store/moduls/notebook'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    notebook
   }
 })
