b360733717a4b9d227ab9e007894a6b371bbb059
miao <miao@163.com>
2021-09-08 06:44:29 +0800

    添加路由模块

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,12 +2,14 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import notebook from '@/store/modules/notebook';
 import note from '@/store/modules/note';
+import user from '@/store/modules/user';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     notebook,
-    note
+    note,
+    user
   }
 });
